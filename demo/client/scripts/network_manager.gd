02e819e81939673f6c5653ad0a370c9af8b8a75a
Brandon Smith <bsmithcompsci@gmail.com>
2024-09-24 21:27:13 -0700

    chore: feature support.

--- demo/client/scripts/network_manager.gd
+++ demo/client/scripts/network_manager.gd
@@ -3,6 +3,7 @@ extends Node
 var controlled_peers: Dictionary = {}
 
 var local_ping: int = 0
+var last_server_response: int = 0
 var ping_delay_secs: int = 5
 var ping_timer: Timer
 var timeout_peer_secs: int = 5
@@ -10,66 +11,66 @@ var timeout_peer_secs: int = 5
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var host: String
-	# if OS.has_feature("dedicated_server"):
-	host = "localhost:19092" # Kafka Server
-	# else:
-	# 	host = "ws://localhost:3000/ws" # Websocket Client
+	if OS.has_feature("dedicated_server"):
+		host = "localhost:19092" # Kafka Server
+	else:
+		host = "ws://localhost:3000/ws" # Websocket Client
 	init_network(host)
 
 func init_network(host: String) -> MultiplayerPeer:
 	var socket: MultiplayerPeer
-	# if OS.has_feature("dedicated_server"):
-	print("Connecting to Kafka Broker: ", host)
-	var kafka: KafkaMultiplayerPeer = KafkaMultiplayerPeer.new()
-	kafka.kafka_log.connect(_on_kafka_logs)
-	# Setup Internal Channels...
-	#	These channels do not communicate from client to server, instead this communicates between server to only edge internally.
-	#	This allows the edge server to tell the game servers that a client has connected or disconnected.
-	var internal_producer_metadata: KafkaPublisherMetadata = KafkaPublisherMetadata.new()
-	internal_producer_metadata.set_brokers(host)
-	internal_producer_metadata.set_topic("internal_to_edge")
-	kafka.register_internal_publisher(internal_producer_metadata)
-	
-	var internal_consumer_metadata: KafkaSubscriberMetadata = KafkaSubscriberMetadata.new()
-	internal_consumer_metadata.set_brokers(host)
-	internal_consumer_metadata.set_topics(["internal_to_game"])
-	internal_consumer_metadata.set_group_id("group-name")
-	internal_consumer_metadata.set_offset_reset(KafkaSubscriberMetadata.OffsetReset.EARLIEST)
-	kafka.register_internal_subscriber(internal_consumer_metadata)
-	
-	# Setup Producer
-	# 	This communicates directly to the clients via kafka topics.
-	var producer_metadata: KafkaPublisherMetadata = KafkaPublisherMetadata.new()
-	producer_metadata.set_brokers(host)
-	producer_metadata.set_topic("server-to-client")
-	kafka.register_publisher(producer_metadata, 1)
-	
-	# Setup Consumer
-	#	This listens from the clients via the kafka topics.
-	var consumer_metadata: KafkaSubscriberMetadata = KafkaSubscriberMetadata.new()
-	consumer_metadata.set_brokers(host)
-	consumer_metadata.set_topics(["client-to-server"])
-	consumer_metadata.set_group_id("group-name")
-	consumer_metadata.set_offset_reset(KafkaSubscriberMetadata.OffsetReset.EARLIEST)
-	kafka.register_subscriber(consumer_metadata, 1)
-	
-	kafka.peer_connected.connect(_server_on_connection)
-	kafka.peer_disconnected.connect(_server_on_disconnection)
-	
-	socket = kafka
-	
-	# Initialize the ping timer for the server.
-	ping_timer = Timer.new()
-	ping_timer.timeout.connect(_on_ping_caller)
-	ping_timer.wait_time = ping_delay_secs
-	add_child(ping_timer)
-	ping_timer.start()
-	# else:
-	# 	print("Connecting to a Websocket URL: ", host)
-	# 	var websocket: WebSocketMultiplayerPeer = WebSocketMultiplayerPeer.new()
-	# 	websocket.create_client(host)
-	# 	websocket.peer_connected.connect(_on_connected)
-	# 	socket = websocket
+	if OS.has_feature("dedicated_server"):
+		print("Connecting to Kafka Broker: ", host)
+		var kafka: KafkaMultiplayerPeer = KafkaMultiplayerPeer.new()
+		kafka.kafka_log.connect(_on_kafka_logs)
+		# Setup Internal Channels...
+		#	These channels do not communicate from client to server, instead this communicates between server to only edge internally.
+		#	This allows the edge server to tell the game servers that a client has connected or disconnected.
+		var internal_producer_metadata: KafkaPublisherMetadata = KafkaPublisherMetadata.new()
+		internal_producer_metadata.set_brokers(host)
+		internal_producer_metadata.set_topic("internal_to_edge")
+		kafka.register_internal_publisher(internal_producer_metadata)
+		
+		var internal_consumer_metadata: KafkaSubscriberMetadata = KafkaSubscriberMetadata.new()
+		internal_consumer_metadata.set_brokers(host)
+		internal_consumer_metadata.set_topics(["internal_to_game"])
+		internal_consumer_metadata.set_group_id("group-name")
+		internal_consumer_metadata.set_offset_reset(KafkaSubscriberMetadata.OffsetReset.EARLIEST)
+		kafka.register_internal_subscriber(internal_consumer_metadata)
+		
+		# Setup Producer
+		# 	This communicates directly to the clients via kafka topics.
+		var producer_metadata: KafkaPublisherMetadata = KafkaPublisherMetadata.new()
+		producer_metadata.set_brokers(host)
+		producer_metadata.set_topic("server-to-client")
+		kafka.register_publisher(producer_metadata, 1)
+		
+		# Setup Consumer
+		#	This listens from the clients via the kafka topics.
+		var consumer_metadata: KafkaSubscriberMetadata = KafkaSubscriberMetadata.new()
+		consumer_metadata.set_brokers(host)
+		consumer_metadata.set_topics(["client-to-server"])
+		consumer_metadata.set_group_id("group-name")
+		consumer_metadata.set_offset_reset(KafkaSubscriberMetadata.OffsetReset.EARLIEST)
+		kafka.register_subscriber(consumer_metadata, 1)
+		
+		kafka.peer_connected.connect(_server_on_connection)
+		kafka.peer_disconnected.connect(_server_on_disconnection)
+		
+		socket = kafka
+		
+		# Initialize the ping timer for the server.
+		ping_timer = Timer.new()
+		ping_timer.timeout.connect(_on_ping_caller)
+		ping_timer.wait_time = ping_delay_secs
+		add_child(ping_timer)
+		ping_timer.start()
+	else:
+		print("Connecting to a Websocket URL: ", host)
+		var websocket: WebSocketMultiplayerPeer = WebSocketMultiplayerPeer.new()
+		websocket.create_client(host)
+		websocket.peer_connected.connect(_on_connected)
+		socket = websocket
 
 	# Set the Multiplayer API to be pointed on the peer above.
 	# At this point, the peer should be polling data.
@@ -115,7 +116,7 @@ func login(username: String, password: String) -> void:
 		printerr("Client should not be processing this message...")
 		return
 	
-	print("Sending Login...")
+	print("Sending Login... %s %s" % [username, password])
 	login_callback.rpc_id(multiplayer.get_remote_sender_id(), true)
 
 func ping_all() -> void:
@@ -130,6 +131,7 @@ func ping_all() -> void:
 func ping(last_timestamp: int) -> void:
 	if multiplayer.is_server():
 		return
+	last_server_response = last_timestamp
 	pong.rpc()
 
 @rpc("any_peer")
